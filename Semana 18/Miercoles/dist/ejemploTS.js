cd7895dd9ba2033acd32fb5552ee7b1a2a2844e6
German Ariza <ger.ariza1998@gmail.com>
2023-10-11 20:52:36 -0600

    Semana 18 (m)

---
+++ Semana 18/Miercoles/dist/ejemploTS.js
@@ -0,0 +1,10 @@
+"use strict";
+let nombre = "Clases miercoles";
+nombre = "90";
+let texto = "Andres";
+let numero = 45;
+let booleano = true;
+function test(nombre) {
+    return "hola" + nombre;
+}
+let carros = ['bmw', 'Nissan', 'Toyota', '12'];
---
+++ Semana 18/Miercoles/src/ejemploTS.ts
@@ -0,0 +1,17 @@
+
+
+//Ejemplo
+let nombre: string = "Clases miercoles";
+
+nombre = "90";
+
+//Tipos de datos
+let texto: string = "Andres";
+let numero: number = 45;
+let booleano: boolean = true;
+
+function test(nombre: string){
+    return "hola"+nombre;
+}
+
+let carros: string[] = ['bmw','Nissan','Toyota', '12'];
